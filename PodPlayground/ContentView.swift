77759d08e1e26f78a00bd217d1e80d6226691c28
Eduardo González <eduardo.gonzalez@getwellat.com>
2022-09-17 12:27:20 +0200

    Initial Commit

---
+++ PodPlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PodPlayground
+//
+//  Created by Eduardo González on 17/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PodPlayground/PodPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodPlaygroundApp.swift
+//  PodPlayground
+//
+//  Created by Eduardo González on 17/9/22.
+//
+
+import SwiftUI
+
+@main
+struct PodPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
